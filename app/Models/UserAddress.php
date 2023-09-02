d4bab684c29608b4a79e9cde3fa41064d04bfbff
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-09-02 16:20:13 -0300

    feat: "wip"

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class UserAddress extends Model
 {
@@ -19,4 +20,9 @@ class UserAddress extends Model
         'city', 
         'uf',
     ];
+
+    public function users(): HasOne
+    {
+        return  $this->hasOne(User::class);
+    }
 }
