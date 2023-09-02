526722488c297e2b8814d0ee33b7579d32544dd1
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-09-02 16:18:38 -0300

    feat: "wip"

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class UserAddress extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'postal_code',
+        'address',
+        'number',
+        'complement', 
+        'neighborhood', 
+        'city', 
+        'uf',
+    ];
 }
