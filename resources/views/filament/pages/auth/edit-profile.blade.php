4d26212f85fde12a364775452ad64b444343c755
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-09-07 23:27:35 -0300

    feat: wip

--- resources/views/filament/pages/auth/edit-profile.blade.php
+++ resources/views/filament/pages/auth/edit-profile.blade.php
@@ -1,5 +1,14 @@
+<div>
 
+    <header class="fi-simple-header py-8">
+        <h1 class="fi-header-heading text-2x1 font-bold tracking-tight text-gray-950 dark:text-white sm:text3x1">
+            My Profile
+        </h1>
 
+        <p class="">
+            Manage your user profile here
+        </p>
+    </header>
 
     <x-filament-panels::form wire:submit="save">
         {{ $this->form }}
@@ -9,3 +18,5 @@
             :full-width="$this->hasFullWidthFormActions()"
         />
     </x-filament-panels::form>
+
+</div>
