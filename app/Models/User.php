f66d069499101a68b07c670d8d75fb53058158dd
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-01-04 22:44:03 -0300

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Sushi\Sushi;
-
-
 
 class User extends Authenticatable
 {
@@ -24,29 +20,26 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'document',
         'password',
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-    
-    
-    public function userAddress(): HasOne
-    {
-        return  $this->hasOne(User::class);
-    }
-
-    public function Address(): HasOne
-    {
-        return  $this->hasOne(Address::class);
-    }
 }
